170636b9a28bb897d7031f085fbc47fbc47d9c5b
Pranali <pranali.mali2014@gmail.com>
2024-09-09 15:26:24 +0530

    Third change

--- src/test/java/pomoarnge/Search.java
+++ src/test/java/pomoarnge/Search.java
@@ -1,10 +1,24 @@
 package pomoarnge;
 
-public class Search {
-/********************/
-	/********************/
-	/********************/
-	/********************/
-	/********************/
-	/********************/
-}
+
+
+
+	import java.util.List;
+
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.support.FindBy;
+	import org.openqa.selenium.support.PageFactory;
+
+
+	public class Search {
+		
+		//Constructor
+		WebDriver driver;
+		
+			public Search(WebDriver driver) {
+				
+				this.driver=driver;
+				PageFactory.initElements(driver, this);
+			}}
+
